a5af0c1ad89c53f21d8bd33ce1bdfcfc5567d51a
Hugh <hugh.wang@mail.mcgill.ca>
2026-03-03 17:47:12 -0500

    Another tcl

--- avalon_bus_tb.tcl
+++ avalon_bus_tb.tcl
@@ -1,33 +1,33 @@
 proc AddWaves {} {
     ;# Add waves for the Avalon MM Interface
-    add wave -position end sim:/avalon_mm_tb/clk
-    add wave -position end sim:/avalon_mm_tb/reset
-    add wave -position end sim:/avalon_mm_tb/op_type
-    add wave -position end sim:/avalon_mm_tb/fsm_addr
-    add wave -position end sim:/avalon_mm_tb/in_block
-    add wave -position end sim:/avalon_mm_tb/out_block
-    add wave -position end sim:/avalon_mm_tb/busy
-    add wave -position end sim:/avalon_mm_tb/mem_waitrequest
-    add wave -position end sim:/avalon_mm_tb/mem_readdata
-    add wave -position end sim:/avalon_mm_tb/mem_addr
-    add wave -position end sim:/avalon_mm_tb/mem_read
-    add wave -position end sim:/avalon_mm_tb/mem_write
-    add wave -position end sim:/avalon_mm_tb/mem_writedata
+    add wave -position end sim:/avalon_bus_tb/clk
+    add wave -position end sim:/avalon_bus_tb/reset
+    add wave -position end sim:/avalon_bus_tb/op_type
+    add wave -position end sim:/avalon_bus_tb/fsm_addr
+    add wave -position end sim:/avalon_bus_tb/in_block
+    add wave -position end sim:/avalon_bus_tb/out_block
+    add wave -position end sim:/avalon_bus_tb/busy
+    add wave -position end sim:/avalon_bus_tb/mem_waitrequest
+    add wave -position end sim:/avalon_bus_tb/mem_readdata
+    add wave -position end sim:/avalon_bus_tb/mem_addr
+    add wave -position end sim:/avalon_bus_tb/mem_read
+    add wave -position end sim:/avalon_bus_tb/mem_write
+    add wave -position end sim:/avalon_bus_tb/mem_writedata
 
     ;# Internal Architecture Signals
-    add wave -position end sim:/avalon_mm_tb/uut/byte_counter
-    add wave -position end sim:/avalon_mm_tb/uut/tmp_busy
-    add wave -position end sim:/avalon_mm_tb/uut/tmp_out_block
+    add wave -position end sim:/avalon_bus_tb/uut/byte_counter
+    add wave -position end sim:/avalon_bus_tb/uut/tmp_busy
+    add wave -position end sim:/avalon_bus_tb/uut/tmp_out_block
 }
 
 vlib work
 
 ;# Compile components
 vcom ./src/avalon_bus.vhd
-vcom ./tests/unit/avalon_mm_tb.vhd
+vcom ./tests/unit/avalon_bus_tb.vhd
 
 ;# Start simulation
-vsim work.avalon_mm_tb
+vsim work.avalon_bus_tb
 
 ;# Add the waves
 AddWaves
