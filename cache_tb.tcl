e48b8e2534c3070a6e44083bf1aec50c8425a829
Hugh <hugh.wang@mail.mcgill.ca>
2026-03-03 17:46:45 -0500

    Fixed some tcl scripts

---
+++ cache_tb.tcl
@@ -0,0 +1,39 @@
+proc AddWaves {} {
+    ;# Add waves for the cache unit
+    add wave -position end sim:/cache_tb/clock
+    add wave -position end sim:/cache_tb/reset
+
+    ;# Avalon slave interface (CPU side)
+    add wave -position end sim:/cache_tb/s_addr
+    add wave -position end sim:/cache_tb/s_read
+    add wave -position end sim:/cache_tb/s_readdata
+    add wave -position end sim:/cache_tb/s_write
+    add wave -position end sim:/cache_tb/s_writedata
+    add wave -position end sim:/cache_tb/s_waitrequest
+
+    ;# Avalon master interface (RAM side)
+    add wave -position end sim:/cache_tb/m_addr
+    add wave -position end sim:/cache_tb/m_read
+    add wave -position end sim:/cache_tb/m_readdata
+    add wave -position end sim:/cache_tb/m_write
+    add wave -position end sim:/cache_tb/m_writedata
+    add wave -position end sim:/cache_tb/m_waitrequest
+
+    ;# Internal Signals (add/remove based on what you actually declare)
+    ;# To declare next
+}
+
+vlib work
+
+;# Compile components
+vcom ./src/cache.vhd
+vcom ./tests/unit/cache_tb.vhd
+
+;# Start simulation
+vsim work.cache_tb
+
+;# Add the waves
+AddWaves
+
+;# Run for 150 ns
+run 150ns
