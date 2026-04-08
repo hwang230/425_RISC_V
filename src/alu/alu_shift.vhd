4242877859c4b385ad3834693da1e753cb0e5689
Joseph Ciaravella <joseph.ciaravella@mail.mcgill.ca>
2026-04-08 17:51:36 -0400

    add shifting logic

---
+++ src/alu/alu_shift.vhd
@@ -0,0 +1,43 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity alu_arithmetic is 
+    port(
+        alu_op: in std_logic_vector(3 downto 0);
+        rs2_val, rs1_val: std_logic_vector(31 downto 0);
+
+        output: out std_logic_vector(31 downto 0)
+    );
+end alu_arithmetic;
+
+-- covers SLL, SRL, SRA
+architecture arch of alu_arithmetic is
+
+    constant ALU_SLL : std_logic_vector(3 downto 0) := "0110";
+    constant ALU_SRL : std_logic_vector(3 downto 0) := "0111";
+    constant ALU_SRA : std_logic_vector(3 downto 0) := "1000";
+
+    signal temp : std_logic_vector(31 downto 0);
+    signal shift_amt : integer range 0 to 31;
+
+begin
+
+    shift_amt <= to_integer(unsigned(rs2_val(4 downto 0)));
+
+    process(alu_op, rs1_val, rs2_val, shift_amt)
+    begin
+
+        case alu_op is
+            when ALU_SLL =>
+                temp <= std_logic_vector(shift_left(unsigned(rs1_val), shift_amt));
+            when ALU_SRL =>
+                temp <= std_logic_vector(shift_right(unsigned(rs1_val), shift_amt));
+            when ALU_SRA =>
+                temp <= std_logic_vector(shift_right(signed(rs1_val), shift_amt));
+            when others =>
+                temp <= (others => '0');
+        end case;
+    end process;
+    output <= temp;
+end arch;
