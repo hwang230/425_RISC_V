e07cb480edfa39f731e30a8804b349c1f4d27abc
Hugh <hugh.wang@mail.mcgill.ca>
2026-04-09 16:11:34 -0400

    removed unneeded files and modified memory to return 4 bytes

--- src/memory.vhd
+++ src/memory.vhd
@@ -11,31 +11,31 @@ ENTITY memory IS
 	);
 	PORT (
 		clock: IN STD_LOGIC;
-		writedata: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-		address: IN INTEGER RANGE 0 TO ram_size-1;
+		writedata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+		address: IN INTEGER RANGE 0 TO 8191;
 		memwrite: IN STD_LOGIC;
 		memread: IN STD_LOGIC;
-		readdata: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+		readdata: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
 		waitrequest: OUT STD_LOGIC
 	);
 END memory;
 
 ARCHITECTURE rtl OF memory IS
-	TYPE MEM IS ARRAY(ram_size-1 downto 0) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
+	TYPE MEM IS ARRAY(0 TO (ram_size/4)-1) OF STD_LOGIC_VECTOR(31 DOWNTO 0);
 	SIGNAL ram_block: MEM := (others => (others => '0'));
-	SIGNAL read_address_reg: INTEGER RANGE 0 to ram_size-1;
+	SIGNAL read_address_reg: INTEGER RANGE 0 TO (ram_size/4)-1;
 	SIGNAL write_waitreq_reg: STD_LOGIC := '1';
 	SIGNAL read_waitreq_reg: STD_LOGIC := '1';
 BEGIN
 	--This is the main section of the SRAM model
 	mem_process: PROCESS (clock)
 	BEGIN
-		--This is a cheap trick to initialize the SRAM in simulation
-		IF(now < 1 ps)THEN
-			For i in 0 to ram_size-1 LOOP
-				ram_block(i) <= std_logic_vector(to_unsigned(i,8));
-			END LOOP;
-		end if;
+		-- --This is a cheap trick to initialize the SRAM in simulation
+		-- IF(now < 1 ps)THEN
+		-- 	For i in 0 to ram_size/4-1 LOOP
+		-- 		ram_block(i) <= std_logic_vector(to_unsigned(i,32));
+		-- 	END LOOP;
+		-- end if;
 
 		--This is the actual synthesizable SRAM block
 		IF (clock'event AND clock = '1') THEN
