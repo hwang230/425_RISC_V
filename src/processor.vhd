91a4e021386b7a8c449ed510640652d8768691d4
Hugh <hugh.wang@mail.mcgill.ca>
2026-04-14 10:45:12 -0400

    Added comments for processor

--- src/processor.vhd
+++ src/processor.vhd
@@ -60,7 +60,7 @@ constant ALU_BGE : std_logic_vector(3 downto 0) := "1101";
 component memory is 
 GENERIC(
     ram_size : INTEGER := 32768;
-    mem_delay : time := 10 ns;
+    mem_delay : time := 1 ns;
     clock_period : time := 1 ns
 );
 PORT (
@@ -150,21 +150,21 @@ signal uses_rs1: STD_LOGIC := '0'; -- whether current decoded instruction reads 
 signal uses_rs2: STD_LOGIC := '0'; -- whether current decoded instruction reads rs2
 
 -- prep immediates to be passed into the alu
-signal alu_imm12 : std_logic_vector(11 downto 0);
-signal alu_imm20 : std_logic_vector(19 downto 0);
+signal alu_imm12: std_logic_vector(11 downto 0);
+signal alu_imm20: std_logic_vector(19 downto 0);
 
 -- signals for calculating branch and jump addresses
-signal branch_taken : std_logic;
-signal jump_taken : std_logic;
-signal flush_taken : std_logic;
-signal id_ex_target_imm : signed(31 downto 0);
-signal target_address : std_logic_vector(31 downto 0);
+signal branch_taken: std_logic;
+signal jump_taken: std_logic;
+signal flush_taken: std_logic;
+signal id_ex_target_imm: signed(31 downto 0);
+signal target_address: std_logic_vector(31 downto 0);
 
 -- signal to load from data mem
-signal load_pending       : std_logic := '0';
-signal pending_rd         : std_logic_vector(4 downto 0) := (others => '0');
-signal pending_reg_write  : std_logic := '0';
-signal pending_alu_output : std_logic_vector(31 downto 0) := (others => '0');
+signal load_pending: std_logic := '0';
+signal pending_rd: std_logic_vector(4 downto 0) := (others => '0');
+signal pending_reg_write: std_logic := '0';
+signal pending_alu_output: std_logic_vector(31 downto 0) := (others => '0');
 
 begin
 -- data memory
@@ -205,7 +205,7 @@ alu_imm20 <=
     id_ex_imm_J when J_TYPE,
     (others => '0') when others;
 
-
+-- determine whether branch or jump is taken
 branch_taken <= '1' when (id_ex_op_type = B_TYPE and ex_ALU_output(0) = '1') else '0';
 jump_taken <= '1' when (id_ex_op_type = J_TYPE or (id_ex_op_type = I_TYPE and id_ex_opcode = OPCODE_JALR)) else '0';
 flush_taken <= '1' when (branch_taken = '1' or jump_taken = '1') else '0';
@@ -219,6 +219,9 @@ id_ex_target_imm <= resize(signed(id_ex_imm_B & '0'), 32) when (id_ex_op_type = 
 
 target_address <= std_logic_vector((signed(id_ex_rs1_val) + resize(signed(id_ex_imm_I), 32)) and to_signed(-2, 32))
                   when (id_ex_op_type = I_TYPE and id_ex_opcode = OPCODE_JALR) else std_logic_vector(to_signed(id_ex_pc, 32) + id_ex_target_imm);
+
+
+-- ALU module declaration and signal assignment
 ALU: entity work.alu
 port map(
     alu_op => id_ex_alu_op,
@@ -235,26 +238,26 @@ port map(
 -- processor pipeline 
 process(clock, reset)
     -- added variables for decode to get the control siganls within the same cycle
-    variable cur_opcode   : STD_LOGIC_VECTOR(6 downto 0);
-    variable cur_funct3   : STD_LOGIC_VECTOR(2 downto 0);
-    variable cur_funct7   : STD_LOGIC_VECTOR(6 downto 0);
-    variable cur_rs1      : STD_LOGIC_VECTOR(4 downto 0);
-    variable cur_rs2      : STD_LOGIC_VECTOR(4 downto 0);
-    variable cur_rd       : STD_LOGIC_VECTOR(4 downto 0);
+    variable cur_opcode: STD_LOGIC_VECTOR(6 downto 0);
+    variable cur_funct3: STD_LOGIC_VECTOR(2 downto 0);
+    variable cur_funct7: STD_LOGIC_VECTOR(6 downto 0);
+    variable cur_rs1: STD_LOGIC_VECTOR(4 downto 0);
+    variable cur_rs2: STD_LOGIC_VECTOR(4 downto 0);
+    variable cur_rd: STD_LOGIC_VECTOR(4 downto 0);
     variable cur_opcode_type : STD_LOGIC_VECTOR(2 downto 0);
-    variable cur_imm_I    : STD_LOGIC_VECTOR(11 downto 0);
-    variable cur_imm_S    : STD_LOGIC_VECTOR(11 downto 0);
-    variable cur_imm_B    : STD_LOGIC_VECTOR(11 downto 0);
-    variable cur_imm_U    : STD_LOGIC_VECTOR(19 downto 0);
-    variable cur_imm_J    : STD_LOGIC_VECTOR(19 downto 0);
-    variable cur_uses_rs1 : STD_LOGIC;
-    variable cur_uses_rs2 : STD_LOGIC;
-    variable cur_id_alu_op : STD_LOGIC_VECTOR(3 downto 0);
-    variable cur_id_mem_read : STD_LOGIC;
-    variable cur_id_mem_write : STD_LOGIC;
-    variable cur_id_reg_write : STD_LOGIC;
-    variable cur_id_mem_to_reg_write : STD_LOGIC;
-    variable stall        : STD_LOGIC;
+    variable cur_imm_I: STD_LOGIC_VECTOR(11 downto 0);
+    variable cur_imm_S: STD_LOGIC_VECTOR(11 downto 0);
+    variable cur_imm_B: STD_LOGIC_VECTOR(11 downto 0);
+    variable cur_imm_U: STD_LOGIC_VECTOR(19 downto 0);
+    variable cur_imm_J: STD_LOGIC_VECTOR(19 downto 0);
+    variable cur_uses_rs1: STD_LOGIC;
+    variable cur_uses_rs2: STD_LOGIC;
+    variable cur_id_alu_op: STD_LOGIC_VECTOR(3 downto 0);
+    variable cur_id_mem_read: STD_LOGIC;
+    variable cur_id_mem_write: STD_LOGIC;
+    variable cur_id_reg_write: STD_LOGIC;
+    variable cur_id_mem_to_reg_write: STD_LOGIC;
+    variable stall: STD_LOGIC;
 begin
     if (reset = '1') then 
         -- reset all intermediate signals here
@@ -480,7 +483,7 @@ begin
         if (flush_taken = '1') then
             pc <= to_integer(unsigned(target_address));
 
-            -- squash younger fetched instruction
+            -- flush younger fetched instruction
             if_id_instr <= (others => '0');
             if_id_pc <= 0;
 
@@ -492,6 +495,7 @@ begin
                 -- Step 1: issue memory read for current PC
                 i_addr <= pc / 4;
                 i_read <= '1';
+                -- to account for one cycle delay from accessing instruction memory
                 fetch_pending <= '1';
 
             elsif (i_waitrequest = '0') then
@@ -502,6 +506,7 @@ begin
                 -- advance PC only after instruction is accepted
                 pc <= pc + 4;
 
+                -- lower the control after finishing the fetch
                 fetch_pending <= '0';
             end if;
         end if;
@@ -509,11 +514,11 @@ begin
         --------------
         -- ID stage --
         -------------- 
-       -- If hazard, should stall until the instruction completes 
 
         -- assign each var the respective value retrieved from fetch 
         -- if stall occurs, if_id_instr will not get updated and the same instruction will
         -- stay in decode until hazard resolved
+        -- Simply latch the signals here
         opcode <= cur_opcode;
         opcode_type <= cur_opcode_type;
         funct3 <= cur_funct3;
@@ -533,6 +538,7 @@ begin
         -- Latch result from DECODE to EXECUTE
         -- when stall occurs, insert NOPs
         if (stall = '1' or flush_taken = '1') then
+            -- insert addi x0, x0, 0 as the bubble instruction in EX stage
             id_ex_alu_op <= "0000";
             id_ex_opcode <= "0000000";
             id_ex_pc <= 0;
@@ -548,9 +554,10 @@ begin
             id_ex_imm_B <= (others => '0');
             id_ex_imm_U <= (others => '0');
             id_ex_imm_J <= (others => '0');
-            id_ex_rd_reg1 <= (others => '0'); -- bubble should not appear as a writer next cycle
+            id_ex_rd_reg1 <= (others => '0'); 
             id_ex_op_type <= (others => '0');
         else
+            -- if no stalls, simply latch the values from decode to execute
             id_ex_alu_op <= cur_id_alu_op;
             id_ex_opcode <= cur_opcode;
             id_ex_pc <= if_id_pc;
@@ -574,7 +581,8 @@ begin
         -- EX stage --
         --------------
         -- latch the control signals for mem stage from decode 
-        -- latch the control signals and relevant intermediate results
+        -- main logic already performed in the ALU module, we just need to latch the 
+        -- results and the controls signals to MEM stage
         ex_mem_mem_read <= id_ex_mem_read;
         ex_mem_mem_write <= id_ex_mem_write;
         ex_mem_rd <= id_ex_rd;
@@ -594,28 +602,36 @@ begin
         d_write <= '0';
         d_read <= '0';
         if load_pending = '1' then
+            -- Second iter goes in here for load
+            -- Data is ready in d_readdata
             mem_wb_read_data <= d_readdata;
             mem_wb_rd <= pending_rd;
             mem_wb_rd_reg3 <= pending_rd;
             mem_wb_reg_write <= pending_reg_write;
             mem_wb_mem_to_reg_write <= '1';
             mem_wb_op_type <= I_TYPE;
+            -- lower the signal to ensure loading finished
             load_pending <= '0';
 
         elsif ex_mem_mem_read = '1' then
+            -- set the addr and read signal
             d_read <= '1';
             d_addr <= to_integer(unsigned(ex_mem_ALU_output(31 downto 2)));
 
+            -- this accounts for the extra cycle required to receive the data back
+            -- First iter goes in here for load
             pending_rd <= ex_mem_rd;
             pending_reg_write <= ex_mem_reg_write;
             pending_alu_output <= ex_mem_ALU_output;
             load_pending <= '1';
 
         elsif ex_mem_mem_write = '1' then
+            -- set the addr, writedata and write signal
             d_write <= '1';
             d_addr <= to_integer(unsigned(ex_mem_ALU_output(31 downto 2)));
             d_writedata <= ex_mem_rs2_val;
 
+            -- latch the signals
             mem_wb_rd <= ex_mem_rd;
             mem_wb_rd_reg3 <= ex_mem_rd;
             mem_wb_ALU_output <= ex_mem_ALU_output;
@@ -623,6 +639,7 @@ begin
             mem_wb_mem_to_reg_write <= ex_mem_mem_to_reg_write;
             mem_wb_op_type <= ex_mem_op_type;
         else
+            -- default: just latch the values
             mem_wb_rd <= ex_mem_rd;
             mem_wb_rd_reg3 <= ex_mem_rd;
             mem_wb_ALU_output <= ex_mem_ALU_output;
